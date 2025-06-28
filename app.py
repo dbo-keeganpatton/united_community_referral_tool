06d94ee258f39f0114667c85f20f1a9214c78c88
Keegan <keeganpatton@gmail.com>
2025-06-28 13:26:18 -0500

    just making things a bit cleaner

--- app.py
+++ app.py
@@ -1,6 +1,16 @@
 import streamlit as st
 
-pg = st.navigation([st.Page("./pages/map.py"), st.Page("./pages/referral_list.py")])
+# This is basically just the site index
+# following standard best practice for multi-page apps
+# created in streamlit, doc below for ref when I forget in 6 months
+# https://docs.streamlit.io/develop/concepts/multipage-apps/page-and-navigation
 
+pg = st.navigation(
+    [
+        st.Page("./pages/map.py"), 
+        st.Page("./pages/referral_list.py"),
+        st.Page("./pages/dashboard.py")
+    ]
+)
 
 pg.run()
