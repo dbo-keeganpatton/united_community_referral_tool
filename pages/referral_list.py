0691d15a26390d0a3b3cd9af0cb9964ce4d9af6b
Keegan <keeganpatton@gmail.com>
2025-06-28 21:27:59 -0500

    add datatable to referral page

--- pages/referral_list.py
+++ pages/referral_list.py
@@ -1,4 +1,15 @@
 import streamlit as st
-
+import pandas as pd
 
 st.title("referrals")
+
+data = pd.read_csv("./static/placeholder_data.csv")
+
+st.data_editor(
+    # Only the Boolean indicator for Completion of the referral
+    # is editable. This is to mitigate potential data loss by
+    # multiple users working with the app concurrently.
+    data=data, 
+    hide_index=True,
+    disabled=("client_name", "ministry", "address")
+)
