829095e972df9d3004192a6206e02c6dbbdabcc1
Keegan <keeganpatton@gmail.com>
2025-06-29 21:38:54 -0500

    add DevOps stuff

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
 import streamlit as st
-
+from dotenv import load_dotenv
 # This is basically just the site index
 # following standard best practice for multi-page apps
 # created in streamlit, doc below for ref when I forget in 6 months
 # https://docs.streamlit.io/develop/concepts/multipage-apps/page-and-navigation
 
+load_dotenv()
 pg = st.navigation(
     [
         st.Page("./pages/service_map.py", title="Service Map"), 
