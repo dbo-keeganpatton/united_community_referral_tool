939711abd61a8b046866acabe00069d762d2b6a8
Keegan <keeganpatton@gmail.com>
2025-07-12 11:47:35 -0500

    Add modal dialog for submission check

--- pages/service_map.py
+++ pages/service_map.py
@@ -3,16 +3,16 @@ from api.gsheet_functions import GoogleSheets
 import streamlit as st
 import pandas as pd
 from datetime import datetime
+
 st.set_page_config(
     layout="wide",
     page_title="Ministry Map",
     page_icon="🗺️"
 )
 
-
-
 conn = GoogleSheets()
 url = 'https://cfn.maps.arcgis.com/apps/instant/lookup/index.html?appid=ffdde7dd21cd4fcabcdf33e01f95e747'
+
 ###############################
 #  Title and Logo Alignment   #
 ###############################
@@ -22,7 +22,6 @@ with title_col1:
     st.subheader("United Community Referral Tool")
 with title_col2:
     st.image(image='./static/muw_logo.png', width=100)
-
 
 with st.container(border=True):
     col1, col2 = st.columns(
@@ -62,52 +61,58 @@ with st.container(border=True):
         description_of_support_needed = st.text_input(label="Description of Support Needed", key="description")
         referral_create_date = str(datetime.now().replace(microsecond=0)) 
         notes = st.text_input(label="Notes", key="notes")
+        
+        # Define the dialog function
+        @st.dialog("Submit Referral to database?")
+        def submit_referral_confirmation():
+            st.write("Are you sure you want to submit this referral?")
+            
+            col1, col2 = st.columns(2)
+            
+            with col1:
+                if st.button("Yes, Submit", type="primary"):
+                    
+                    pre_insert_data = conn.Going_To_Get_Data()
+                
+                    new_record = pd.DataFrame({
+                        "student_first_name"               : [student_first_name], 
+                        "student_last_name"                : [student_last_name],
+                        "hoh_first_name"                   : [hoh_first_name],
+                        "hoh_last_name"                    : [hoh_last_name],
+                        "hoh_address"                      : [hoh_address],
+                        "hoh_zip"                          : [hoh_zip],
+                        "hoh_email"                        : [hoh_email],
+                        "hoh_cell_number"                  : [hoh_cell_number],
+                        "frsyc_name"                       : [frsyc_name],
+                        "school"                           : [school],
+                        "ministry"                         : [ministry],
+                        "description_of_support_needed"    : [description_of_support_needed],
+                        "status"                           : "Open",
+                        "referral_create_date"             : [referral_create_date], 
+                        "notes"                            : [notes] if notes else ""
+                    })
 
+                    add_row_to_data = pd.concat([pre_insert_data, new_record], ignore_index=True)
+                    conn.Update_Data(add_row_to_data)
+                    
+                    st.success("Data Added, Check Referral Page for Updated entry.")
+                    st.rerun()
+                    
+            with col2:
+                if st.button("Cancel"):
+                    st.rerun()
 
-        if st.button(label="submit", key="map_submit_button"):
-
+        # Main submit button logic
+        if st.button(label="Submit", key="map_submit_button", type="primary"):
             if (
                 # these fields must be complete for the form submission to work
-                # Notes is optional, since that will primarily be used by 
-                # ministry volunteers.
                 student_first_name and student_last_name and hoh_first_name and 
                 hoh_last_name and hoh_address and hoh_zip and
                 hoh_email and hoh_cell_number and frsyc_name and 
                 school and ministry and description_of_support_needed and 
                 referral_create_date
             ):
-               
-                # All text fields must be filled in for submission to work
-                # I am doing a complete DROP AND REPLACE here...
-                # this action creates two dataframes, one for the original state 
-                # of the data in google sheets and one for the single row that 
-                # will be added. These are then concatenated through a standard 
-                # pandas method, and the sheet is repopulated with the new data.
-                # Yep, this sucks... I'll find a better way at some point.
-
-                pre_insert_data = conn.Going_To_Get_Data()
-            
-                new_record = pd.DataFrame({
-                    "student_first_name"               : [student_first_name], 
-                    "student_last_name"                : [student_last_name],
-                    "hoh_first_name"                   : [hoh_first_name],
-                    "hoh_last_name"                    : [hoh_last_name],
-                    "hoh_address"                      : [hoh_address],
-                    "hoh_zip"                          : [hoh_zip],
-                    "hoh_email"                        : [hoh_email],
-                    "hoh_cell_number"                  : [hoh_cell_number],
-                    "frsyc_name"                       : [frsyc_name],
-                    "school"                           : [school],
-                    "ministry"                         : [ministry],
-                    "description_of_support_needed"    : [description_of_support_needed],
-                    "status"                           : "Open",
-                    "referral_create_date"             : [referral_create_date], 
-                    "notes"                            : ""
-                })
-
-                add_row_to_data = pd.concat([pre_insert_data, new_record], ignore_index=True)
-                conn.Update_Data(add_row_to_data)
-
-                st.success("Data Added, Check Referral Page for Updated entry.")
+                
+                submit_referral_confirmation()
             else:
                 st.warning("All fields must be completed to submit")
